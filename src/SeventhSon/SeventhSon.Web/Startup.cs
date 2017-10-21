4a54fecfdedd3164f101c165c032fc6c4373fd4a
Ievgen Zubkov <euginzub@mail.ru>
2017-10-21 22:28:14 +0300

    Add swagger

--- src/SeventhSon/SeventhSon.Web/Startup.cs
+++ src/SeventhSon/SeventhSon.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SeventhSon.Web
 {
@@ -24,6 +25,11 @@ namespace SeventhSon.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Seventh Son API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,8 +39,15 @@ namespace SeventhSon.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
+                        
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Seventh Son API V1");
+            });
         }
     }
 }
