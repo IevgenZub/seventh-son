65d4222d8859eab538c47b53d8796648c0c21f8e
Ievgen Zubkov <euginzub@mail.ru>
2017-10-22 18:20:45 +0300

    Add separate MVC solution with authentication

---
+++ src/Seventh-Son/Seventh-Son.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Seventh_Son.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
